1e66a1a5f1e12e4de0d6b35956bffd48be57ff59
Billy Mohajeri <billy.mohajeri@gmail.com>
2024-08-06 17:54:44 +0300

    Add product service & controller

--- src/main/java/com/backend/ecommerce/controllers/ProductController.java
+++ src/main/java/com/backend/ecommerce/controllers/ProductController.java
@@ -1,4 +1,23 @@
 package com.backend.ecommerce.controllers;
 
+import com.backend.ecommerce.entities.Product;
+import com.backend.ecommerce.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
+  @Autowired
+  private ProductService productService;
+
+  @PostMapping("/products")
+  public void createProduct(@RequestBody Product product) {
+    productService.createProduct(product);
+  }
+
+
 }
---
+++ src/main/java/com/backend/ecommerce/services/ProductService.java
@@ -0,0 +1,12 @@
+package com.backend.ecommerce.services;
+
+import com.backend.ecommerce.entities.Product;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+  public Product createProduct(Product product) {
+//    Add new product to the database
+    return product;
+  }
+}
